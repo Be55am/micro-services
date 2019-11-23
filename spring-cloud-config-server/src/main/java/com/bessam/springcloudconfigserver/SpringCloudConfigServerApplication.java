de2aca1e5bf868d58ae401402a6b5de953f6d5c1
bessam <bsahli@wissal-sys.com>
2019-11-23 12:27:14 +0100

    added a configuration file to the limits service

--- spring-cloud-config-server/src/main/java/com/bessam/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/bessam/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,7 +2,12 @@ package com.bessam.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * we need to add this annotation to enable the configuration server
+ */
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
