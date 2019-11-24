0b64c67feb1836196104473329fd60b35f3b11ed
bessam <bsahli@wissal-sys.com>
2019-11-24 13:48:43 +0100

    added currency exchage service

---
+++ currency-exchange-service/src/main/java/com/bessam/currencyexchangeservice/CurrencyExchageService.java
@@ -0,0 +1,14 @@
+package com.bessam.currencyexchangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CurrencyExchageService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyExchageService.class, args);
+    }
+
+}
