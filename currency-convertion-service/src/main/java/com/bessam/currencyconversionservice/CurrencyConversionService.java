4ff0526c8d13bf6d9cd9322133d7076c6c7c84cc
Bessam <sahlibessam@gmail.com>
2019-11-25 16:32:14 +0100

    added eureka service discovery and registered services on it and used it

--- currency-convertion-service/src/main/java/com/bessam/currencyconversionservice/CurrencyConversionService.java
+++ currency-convertion-service/src/main/java/com/bessam/currencyconversionservice/CurrencyConversionService.java
@@ -2,12 +2,14 @@ package com.bessam.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 //enable feign to scan for clients and we pass the packages we wants to scan
 @EnableFeignClients("com.bessam.currencyconversionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionService {
 
     public static void main(String[] args) {
